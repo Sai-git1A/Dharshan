dba52f8b78ef5249b1c6ac013175a7a04592e021
Sai Dharshan <saidharshan018@gmail.com>
2023-12-12 09:14:15 +0530

    Added New Data

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -8,12 +8,12 @@ export default function Home() {
     const [weather, setWeather] = useState(null);
 
     const fetchLocationKey = async (location) => {
-        const resLocationKey = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.REACT_APP_WEATHER_KEY}&q=${location.lat},${location.lon}`)
+        const resLocationKey = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.REACT_APP_WEATHER_KEY}&q=${location.lat},${location.lon}`)
         setLocationKey(resLocationKey.data);
     }
 
     const fetchWeather = async (location_key) => {
-        const resWeather = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${location_key}?apikey=${process.env.REACT_APP_WEATHER_KEY}&details=true`)
+        const resWeather = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${location_key}?apikey=${process.env.REACT_APP_WEATHER_KEY}&details=true`)
         setWeather(resWeather.data);
         console.log(resWeather.data[0]);
     }
@@ -37,10 +37,12 @@ export default function Home() {
         {location_key && <div className='location-key'>
             <h2 id='SubTitle' className='home-subtitle'>Location Details</h2>
             <p>Key: {location_key.Key}</p>
-            <p>LocalizedName: {location_key.LocalizedName}</p>
-            <p>AdministrativeArea: {location_key.AdministrativeArea.LocalizedName}</p>
-            <p>Country: {location_key.Country.LocalizedName}</p>
-            <p>TimeZone: {location_key.TimeZone.Code}</p>
+            <p>Name: {location_key.LocalizedName}</p>
+            <p>Dist: {location_key.SupplementalAdminAreas[0].LocalizedName}</p>
+            <p>State: {location_key.AdministrativeArea.LocalizedName} {location_key.AdministrativeArea.ID}</p>
+            <p>Country: {location_key.Country.LocalizedName} {location_key.Country.ID}</p>
+            <p>TimeZone: {location_key.TimeZone.Code} {location_key.TimeZone.Name} {location_key.TimeZone.GmtOffset}</p>
+            <p>Continent: {location_key.Region.LocalizedName} {location_key.Region.ID}</p>
             <button className='btn get-weather' onClick={() => fetchWeather(location_key.Key)}>Get Weather</button>
         </div>}
         {weather && <div className='location-weather'>
