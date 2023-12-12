197ad342fb7d604419269ecdd6ac6411c4f19c6a
Sai Dharshan <saidharshan018@gmail.com>
2023-12-12 18:55:27 +0530

    Some Major UI Updates.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,50 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import CircularProgress from '@mui/joy/CircularProgress';
 import './Home.css';
 
 export default function Home() {
+    const [loading, setLoading] = useState(false);
     const [location, setLocation] = useState(null);
     const [location_key, setLocationKey] = useState(null);
     const [weather, setWeather] = useState(null);
 
     const fetchLocationKey = async (location) => {
+        setLoading(!loading);
         const resLocationKey = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.REACT_APP_WEATHER_KEY}&q=${location.lat},${location.lon}`)
         setLocationKey(resLocationKey.data);
+        setLoading(false);
     }
 
     const fetchWeather = async (location_key) => {
+        setLoading(!loading);
         const resWeather = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${location_key}?apikey=${process.env.REACT_APP_WEATHER_KEY}&details=true`)
         setWeather(resWeather.data);
-        console.log(resWeather.data[0]);
+        setLoading(false);
     }
 
-    useEffect(() => {
+    const getLocation = () => {
+        setLoading(!loading);
         navigator.geolocation.getCurrentPosition(position => {
             setLocation({lat:position.coords.latitude, lon:position.coords.longitude});
         });
-    }, []);
+        setLoading(false);
+    }
 
     return (
         <>
         <h1 id='Title' className='home-title'>Weather</h1>
         <div className='main'>
-        {location && <div className='location'>
+        {location ? '' : <div className='location'><button className='btn get-location' onClick={getLocation}>{loading ? <><CircularProgress color="neutral" variant="soft" size="sm" /> <span>Getting Location...</span></> : 'Get Location'}</button></div>}
+        {location_key ? '' : <>
+        {location && <div className='location-details'>
             <h2 id='SubTitle' className='home-subtitle'>Geo Position</h2>
             <p>Latitude: {location.lat}</p>
             <p>Longitude: {location.lon}</p>
-            <button className='btn get-location' onClick={() => fetchLocationKey(location)}>Get Location</button>
+            <button className='btn get-location-details' onClick={() => fetchLocationKey(location)}>{loading ? <><CircularProgress color="neutral" variant="soft" size="sm" /> <span>Getting Location Details...</span></> : 'Get Location Details'}</button>
         </div>}
+        </>}
+        {weather ? '' : <>
         {location_key && <div className='location-key'>
             <h2 id='SubTitle' className='home-subtitle'>Location Details</h2>
-            <p>Key: {location_key.Key}</p>
             <p>Name: {location_key.LocalizedName}</p>
             <p>Dist: {location_key.SupplementalAdminAreas[0].LocalizedName}</p>
             <p>State: {location_key.AdministrativeArea.LocalizedName} {location_key.AdministrativeArea.ID}</p>
             <p>Country: {location_key.Country.LocalizedName} {location_key.Country.ID}</p>
             <p>TimeZone: {location_key.TimeZone.Code} {location_key.TimeZone.Name} {location_key.TimeZone.GmtOffset}</p>
             <p>Continent: {location_key.Region.LocalizedName} {location_key.Region.ID}</p>
-            <button className='btn get-weather' onClick={() => fetchWeather(location_key.Key)}>Get Weather</button>
+            <button className='btn get-weather' onClick={() => fetchWeather(location_key.Key)}>{loading ? <><CircularProgress color="neutral" variant="soft" size="sm" /> <span>Getting Weather...</span></> : 'Get Weather'}</button>
         </div>}
+        </>}
         {weather && <div className='location-weather'>
             <h2 id='SubTitle' className='home-subtitle'>Current Condition</h2>
             <p>WeatherIcon: {weather[0].WeatherIcon}</p>
